958e5db3cb7ef3a771d4154d0ae57ec2d1c15f00
Dmitrii <dmitriimaiorov.dev@gmail.com>
2025-05-31 22:40:05 +0200

    Изменено приветствие в ветке conflict-test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет из main ветки!");
+        System.out.println("Привет из ветки conflict-test!");
     }
 }
